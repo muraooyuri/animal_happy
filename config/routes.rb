05de5b0113abedf81ecafa3c47cfa7b635dcb6a8
muraooyuri <yuri.0112hm@icloud.com>
2023-03-29 10:02:28 +0000

    コントローラとビューを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'animals/index'
+  get 'animals/show'
+  get 'animals/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'homes/top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
