95d944a2c4b41980bb05f2e1059736555792a194
muraooyuri <yuri.0112hm@icloud.com>
2023-04-04 09:28:46 +0000

    routes.rbの記述追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,27 @@
 Rails.application.routes.draw do
-  get 'animals/index'
-  get 'animals/show'
-  get 'animals/edit'
+  
+  root to: 'homus#top'
+  
+  # get 'animals/index'
+  # get 'animals/show'
+  # get 'animals/edit'
   get 'users/index'
   get 'users/show'
   get 'users/edit'
   get 'homes/top'
+  
   devise_for :users
+  resources :animals, only: [:create, :new, :index, :show, :destroy, :edit, :update] do
+    resources :animal_comments, only: [:create, :destroy]
+  end
+  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    get '/users', to: 'devise/registrations#new'
+  end
+  
+  namespace :genres do
+  get 'toukens/index'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
