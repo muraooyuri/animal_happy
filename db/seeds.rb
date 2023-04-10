e82a06928068bec745d649d58a2dbd281fb12f87
muraooyuri <yuri.0112hm@icloud.com>
2023-04-10 09:00:47 +0000

    ジャンル記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+%i[ 
+  子犬
+  小型犬
+  中型犬
+  大型犬
+  子猫
+  猫
+  鳥
+  爬虫類
+  その他
+].each { |name| Genre.find_or_create_by(name:) }
+genre_ids = Genre.all.pluck(:id)
