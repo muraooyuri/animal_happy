56aa2ea71222c25c48c038c9cd395f5f585de827
muraooyuri <yuri.0112hm@icloud.com>
2023-04-19 10:52:15 +0000

    記述追加

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,9 @@
 class Animal < ApplicationRecord
+  has_many :favorites, dependent: :destroy
+  has_many :anima_comments, dependent: :destroy
+  belongs_to :genre
+  has_many :categories
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
