62bd8155a6509faa5e24d7f934fd4d6e10f3faa5
muraooyuri <yuri.0112hm@icloud.com>
2023-05-12 08:41:37 +0000

    記述の追加と編集

--- db/migrate/20230413082321_create_favorites.rb
+++ db/migrate/20230413082321_create_favorites.rb
@@ -1,8 +1,11 @@
 class CreateFavorites < ActiveRecord::Migration[6.1]
   def change
     create_table :favorites do |t|
+      t.integer :user_id
+      t.integer :animal_id
 
       t.timestamps
+      t.index [:user_id, :animal_id], unique: true
     end
   end
 end
