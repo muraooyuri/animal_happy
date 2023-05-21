ad6426ffb1d460b7d6d7a2402f4da98e1f5525ef
muraooyuri <yuri.0112hm@icloud.com>
2023-05-21 08:29:18 +0000

    記述内容の追加to編集

--- db/migrate/20230330090630_create_animals.rb
+++ db/migrate/20230330090630_create_animals.rb
@@ -1,9 +1,7 @@
 class CreateAnimals < ActiveRecord::Migration[6.1]
   def change
     create_table :animals do |t|
-      t.string :title
-      t.integer :user_id
-      t.text :body
+      
       t.timestamps
     end
   end
